04984ef8b396ee0ca9a9cdbf5d23a904943bc273
abdulhaseeb13mar <abdulhaseeb13mar@gmail.com>
2024-11-16 01:29:22 +0500

    fix: add navigation back button in input search for node path

--- src/Components/inputSearch.tsx
+++ src/Components/inputSearch.tsx
@@ -1,4 +1,6 @@
 import CachedService from "Classes/cachedService";
+import MainLayout from "Layout/mainLayout";
+import { MoveLeft } from "lucide-react";
 import { dispatch, useSelector } from "Store";
 import { setInputSearch } from "Store/Reducers/session";
 
@@ -12,20 +14,33 @@ const InputSearch = () => {
   };
 
   return (
-    <div className="flex flex-col sm:flex-row items-center justify-center gap-3 px-2">
-      <input
-        value={inputSearch}
-        onChange={(e) => dispatch(setInputSearch(e.target.value))}
-        className="border border-white text-accent max-w-[400px] p-2 rounded-lg w-full"
-        placeholder="search validator address"
-      />
-      <div
-        className="btn btn-accent text-base-100 w-full max-w-[400px] sm:w-auto"
-        onClick={handleSearchClick}
-      >
-        Search
+    <>
+      {window.location.pathname.includes("/node") && (
+        <MainLayout>
+          <div
+            className="flex gap-2 mb-2 cursor-pointer"
+            onClick={() => CachedService.navigation(window.location.origin)}
+          >
+            <MoveLeft className="text-secondary" />
+            <p className="text-secondary underline">Back</p>
+          </div>
+        </MainLayout>
+      )}
+      <div className="flex flex-col sm:flex-row items-center justify-center gap-3 px-2">
+        <input
+          value={inputSearch}
+          onChange={(e) => dispatch(setInputSearch(e.target.value))}
+          className="border border-white text-accent max-w-[400px] p-2 rounded-lg w-full"
+          placeholder="search validator address"
+        />
+        <div
+          className="btn btn-accent text-base-100 w-full max-w-[400px] sm:w-auto"
+          onClick={handleSearchClick}
+        >
+          Search
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
